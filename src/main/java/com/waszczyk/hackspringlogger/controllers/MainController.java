e05121d5f6b0b0ae9e6cc004452907c8312597d6
Tomasz Waszczyk <tomasz@waszczyk.com>
2014-07-08 01:43:16 +0200

    [Added] Hacked logger.

--- src/main/java/com/waszczyk/hackspringlogger/controllers/MainController.java
+++ src/main/java/com/waszczyk/hackspringlogger/controllers/MainController.java
@@ -1,13 +1,22 @@
 package com.waszczyk.hackspringlogger.controllers;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.waszczyk.hackspringlogger.utils.AutowiredLogger;
+
 @Controller
 public class MainController {
 
+	
+	//hacked logger
+	
+	@AutowiredLogger
+	Logger logger;
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String printWelcome(ModelMap model) {
 
