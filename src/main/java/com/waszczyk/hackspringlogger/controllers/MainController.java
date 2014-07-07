61f7d818bdf08a2f309089cc6e318f03e2d10e4a
Tomasz Waszczyk <tomasz@waszczyk.com>
2014-07-08 01:28:16 +0200

    [Created] Project && first commit.

---
+++ src/main/java/com/waszczyk/hackspringlogger/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.waszczyk.hackspringlogger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Hello Hack Spring Logger Tutorial ;-)");
+		return "hello";
+	}
+
+}
